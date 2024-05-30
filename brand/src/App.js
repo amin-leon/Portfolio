7719ffe40037b7cfee09d0cfa273739aa67aef69
NPL Codes <104311071+amin-leon@users.noreply.github.com>
2024-05-30 12:21:31 +0200

    setup

---
+++ brand/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-h1 font-bold">This is an H1 Heading</h1>
+      <h2 className="text-h2 font-bold">This is an H2 Heading</h2>
+      <h3 className="text-h3 font-bold">This is an H3 Heading</h3>
+      <h4 className="text-h4 font-bold">This is an H4 Heading</h4>
+      <h5 className="text-h5 font-bold">This is an H5 Heading</h5>
+      <h6 className="text-h6 font-bold">This is an H6 Heading</h6>
+    </div>
+  );
+}
+
+export default App;
---
+++ brand/tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust according to your project structure
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#a6d4fa',
+          DEFAULT: '#2196f3',
+          dark: '#1769aa',
+        },
+        secondary: {
+          light: '#ffb74d',
+          DEFAULT: '#ff9800',
+          dark: '#f57c00',
+        },
+      },
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      fontSize: {
+        'h1': ['2.25rem', { lineHeight: '2.5rem' }], // 36px
+        'h2': ['1.875rem', { lineHeight: '2.25rem' }], // 30px
+        'h3': ['1.5rem', { lineHeight: '2rem' }], // 24px
+        'h4': ['1.25rem', { lineHeight: '1.75rem' }], // 20px
+        'h5': ['1rem', { lineHeight: '1.5rem' }], // 16px
+        'h6': ['0.875rem', { lineHeight: '1.25rem' }], // 14px
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+        // Add custom spacing values
+      },
+      borderRadius: {
+        '4xl': '2rem',
+        // Add custom border radius values
+      },
+      // Extend other settings as needed
+    },
+  },
+  plugins: [],
+};
+
+
